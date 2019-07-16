34dff9a46112e1c820fdbe34396f76d6e28038fb
github username <github email address>
2019-07-16 17:39:08 -0500

    added generic routes to help with layout and made layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :blog_comments
   resources :blogs
+  
+  get '/', to: 'application#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
