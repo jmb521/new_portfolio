9f3df99a7015c884748113aca05cc76d79d6016a
github username <github email address>
2019-07-17 00:31:06 -0500

    updated rails and added activestorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :blog_comments
-  resources :blogs
-  
-  get '/', to: 'application#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
