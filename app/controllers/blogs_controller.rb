667c0be52def9a2b27ef65400f15fd929b69e259
github username <github email address>
2019-07-17 01:17:45 -0500

    added active_storage and action_text

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,22 @@
 class BlogsController < ApplicationController
+
+    def index
+        @blogs = Blog.all
+    end
+    
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
     belongs_to :user
     has_many :blog_comments
+    has_rich_text :content
 end
